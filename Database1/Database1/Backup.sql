840e51e65203d9b8be71071920bbe17819e6c2ac
Hüseyin Demir <huseyin.demir@trendyol.com>
2017-06-22 13:43:48 +0300

    Full Backup Edition

---
+++ Database1/Database1/Backup.sql
@@ -0,0 +1,32 @@
+﻿CREATE PROCEDURE dbo.BackupALL  @path VARCHAR(256) --Backup Path
+AS
+DECLARE @name VARCHAR(50) -- Database Name 
+DECLARE @fileName VARCHAR(256) -- File Name/Data  
+DECLARE @fileDate VARCHAR(20) -- DateTime Stamp for File Name
+
+
+--Time Stamp for Files
+SELECT @fileDate = CONVERT(VARCHAR(20),GETDATE(),112) 
+
+DECLARE db_cursor CURSOR FOR  
+
+--All Databases Except System Databases
+SELECT name 
+FROM master.dbo.sysdatabases 
+WHERE name NOT IN ('master','model','msdb','tempdb','ReportServer','ReportServerTempDB')  
+
+
+OPEN db_cursor   
+FETCH NEXT FROM db_cursor INTO @name   
+--Backup Starts
+WHILE @@FETCH_STATUS = 0   
+BEGIN   
+       SET @fileName = @path + @name + '.BAK'  
+       BACKUP DATABASE @name TO DISK = @fileName WITH STATS = 1,COMPRESSION,CHECKSUM,INIT
+
+       FETCH NEXT FROM db_cursor INTO @name   
+END   
+--Backup End
+CLOSE db_cursor   
+DEALLOCATE db_cursor 
+GO
