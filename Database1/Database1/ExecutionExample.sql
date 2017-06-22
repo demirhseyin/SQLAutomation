f050be899e2045d94719127c66e1d40b8d6edc67
Hüseyin Demir <huseyin.demir@trendyol.com>
2017-06-22 15:59:42 +0300

    Procedure Examples Implemented

---
+++ Database1/Database1/ExecutionExample.sql
@@ -0,0 +1,8 @@
+﻿--Log Backup Procedure Execution
+
+exec dbo.LogBackup @path ='C:\DBA\PF-00ZPPV'
+
+--Full Backup Procedure Execution
+
+exec dbo.BackupALL @path ='X:\DBA_BACKUP\ONEADMIN\ONEADMIN'
+
