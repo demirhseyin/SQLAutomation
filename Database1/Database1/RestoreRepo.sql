1389ec02838ef33856802954606c3f023ec651ea
Hüseyin Demir <huseyin.demir@trendyol.com>
2017-06-23 15:43:03 +0300

    Restore Repository Version 1.0

---
+++ Database1/Database1/RestoreRepo.sql
@@ -0,0 +1,16 @@
+﻿--Restore an Existing Database(Overwrite)
+
+USE [master]
+RESTORE DATABASE [NameGenerator] FROM  DISK = N'Location of Backup' WITH  FILE = 1,  NORECOVERY,  NOUNLOAD,  STATS = 5
+GO
+
+--Restore a Database with Different Name(Clone)
+
+USE [master]
+--Get The Logical Names of Database Files
+RESTORE FILELISTONLY FROM DISK='Location of Backup'
+--Restore The Database
+RESTORE DATABASE [Database_Name] FROM  DISK = N'Location of Backup' WITH  FILE = 1,  
+MOVE N'NameGenerator' TO N'Location of The New .MDF File',  
+MOVE N'NameGenerator_log' TO N'Location of The New .LDF File',  NOUNLOAD,  STATS = 5
+GO
